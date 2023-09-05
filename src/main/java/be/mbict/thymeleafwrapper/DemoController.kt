a89788665960b99f3b3c347ec4439799ec5517f6
Marc Bogaerts <marc.je.bogaerts@telenet.be>
2023-09-05 19:55:15 +0200

    Model is kotlin data class

--- src/main/java/be/mbict/thymeleafwrapper/DemoController.kt
+++ src/main/java/be/mbict/thymeleafwrapper/DemoController.kt
@@ -1,11 +1,20 @@
 package be.mbict.thymeleafwrapper
 
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class DemoController {
 
     @GetMapping("/hello")
-    fun hello() = "hello"
+    fun hello(model: Model): String {
+        model.addAttribute("greeting", Greeting(title = "Salvus, visitor", salute = "Let's take some time to exchange greetings."))
+        return "hello"
+    }
 }
+
+data class Greeting(
+    val title: String,
+    val salute: String
+)
