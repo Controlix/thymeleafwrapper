209d9f173eea087c93965fae4adac2b469f4c180
Marc Bogaerts <marc.je.bogaerts@telenet.be>
2023-09-05 19:29:52 +0200

    Empty project

---
+++ src/main/java/be/mbict/thymeleafwrapper/ThymeleafwrapperApplication.java
@@ -0,0 +1,13 @@
+package be.mbict.thymeleafwrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafwrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafwrapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/mbict/thymeleafwrapper/ThymeleafwrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package be.mbict.thymeleafwrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafwrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
